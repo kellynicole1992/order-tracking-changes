d0f6678ae7ec543b4f6f40887a62c5eb793eb52a
kellynicole1992 <64326430+kellynicole1992@users.noreply.github.com>
2020-05-25 14:33:46 -0400

    Update reconcile.py

--- reconcile.py
+++ reconcile.py
@@ -4,18 +4,16 @@ import argparse
 import lib.donations
 from lib import clusters
 import sys
-import time
 from tqdm import tqdm
 import yaml
 from lib.cancelled_items_retriever import CancelledItemsRetriever
+from lib.config import open_config
 from lib.order_info import OrderInfo, OrderInfoRetriever
 from lib.group_site_manager import GroupSiteManager
 from lib.driver_creator import DriverCreator
 from lib.reconciliation_uploader import ReconciliationUploader
 from lib.tracking_output import TrackingOutput
 from lib.tracking_uploader import TrackingUploader
-
-CONFIG_FILE = "config.yml"
 
 
 def fill_costs(all_clusters, config):
@@ -26,7 +24,6 @@ def fill_costs(all_clusters, config):
     for order_id in cluster.orders:
       try:
         order_info = order_info_retriever.get_order_info(order_id)
-        #print(order_info)
       except Exception as e:
         print(
             f"Exception when getting order info for {order_id}. Please check the oldest email associated with that order. Skipping..."
@@ -39,8 +36,7 @@ def fill_costs(all_clusters, config):
 def fill_email_ids(all_clusters, config):
   order_info_retriever = OrderInfoRetriever(config)
   total_orders = sum([len(cluster.orders) for cluster in all_clusters])
-  with tqdm(
-      desc='Fetching order costs', unit='order', total=total_orders) as pbar:
+  with tqdm(desc='Fetching order costs', unit='order', total=total_orders) as pbar:
     for cluster in all_clusters:
       cluster.expected_cost = 0.0
       cluster.email_ids = set()
@@ -76,7 +72,6 @@ def get_new_tracking_pos_costs_maps(config, group_site_manager, args):
     trackings_to_costs_map.update(group_trackings_to_po)
     po_to_cost_map.update(group_po_to_cost)
     trackings_to_po_map.update(trackings_to_po)
-    #print(f"po to group tracking: {group_trackings_to_po}")
 
   return (trackings_to_po_map, trackings_to_costs_map, po_to_cost_map)
 
@@ -89,7 +84,7 @@ def map_clusters_by_tracking(all_clusters):
   return result
 
 
-def merge_by_trackings_tuples(clusters_by_tracking, trackings_to_cost):
+def merge_by_trackings_tuples(clusters_by_tracking, trackings_to_cost, all_clusters):
   for trackings_tuple, cost in trackings_to_cost.items():
     if len(trackings_tuple) == 1:
       continue
@@ -109,6 +104,7 @@ def merge_by_trackings_tuples(clusters_by_tracking, trackings_to_cost):
     for other_cluster in cluster_list[1:]:
       if not (other_cluster.trackings.issubset(first_cluster.trackings) and
               other_cluster.orders.issubset(first_cluster.orders)):
+        all_clusters.remove(other_cluster)
         first_cluster.merge_with(other_cluster)
     for tracking in trackings_tuple:
       clusters_by_tracking[tracking] = first_cluster
@@ -134,12 +130,11 @@ def fill_costs_new(clusters_by_tracking, trackings_to_cost, po_to_cost, args):
           cluster.non_reimbursed_trackings.remove(tracking)
 
   # Next, manual PO fixes
- # for cluster in clusters_by_tracking.values():
- #   pos = cluster.purchase_orders
- #   if pos:
- #     for po in pos:
- #       cluster.tracked_cost += float(po_to_cost.get(po, 0.0))
-
+#  for cluster in clusters_by_tracking.values():
+#    pos = cluster.purchase_orders
+#    if pos:
+#      for po in pos:
+#        cluster.tracked_cost += float(po_to_cost.get(po, 0.0))
 
 
 def fill_cancellations(all_clusters, config):
@@ -174,12 +169,10 @@ def reconcile_new(config, args):
   driver_creator = DriverCreator()
   group_site_manager = GroupSiteManager(config, driver_creator)
 
-  trackings_to_po, trackings_to_cost, po_to_cost = get_new_tracking_pos_costs_maps(
-      config, group_site_manager, args)
+  trackings_to_po, trackings_to_cost, po_to_cost = get_new_tracking_pos_costs_maps(config, group_site_manager, args)
 
   clusters_by_tracking = map_clusters_by_tracking(all_clusters)
-
-  merge_by_trackings_tuples(clusters_by_tracking, trackings_to_cost)
+  merge_by_trackings_tuples(clusters_by_tracking, trackings_to_cost, all_clusters)
 
   fill_costs_new(clusters_by_tracking, trackings_to_cost, po_to_cost, args)
 
@@ -187,32 +180,30 @@ def reconcile_new(config, args):
   fill_purchase_orders(all_clusters, trackings_to_po, args)
   reconciliation_uploader.download_upload_clusters_new(all_clusters)
 
+def fill_purchase_orders(all_clusters, tracking_to_po, args): 
+    print("Filling purchase orders")  
+    
+    for cluster in all_clusters:  
+      if args.groups and cluster.group not in args.groups:  
+        continue  
+    
+      #cluster.non_reimbursed_trackings = set(cluster.trackings)  
+      for tracking in cluster.trackings:  
+        if tracking in tracking_to_po:  
+          cluster.purchase_orders.add(tracking_to_po[tracking]) 
+          #cluster.non_reimbursed_trackings.remove(tracking)  
+    
+  
 
 def main():
   parser = argparse.ArgumentParser(description='Reconciliation script')
   parser.add_argument("--groups", nargs="*")
   args, _ = parser.parse_known_args()
 
-  print(f"args: {args.groups}")
-
-  with open(CONFIG_FILE, 'r') as config_file_stream:
-    config = yaml.safe_load(config_file_stream)
+  config = open_config()
 
   reconcile_new(config, args)
 
-def fill_purchase_orders(all_clusters, tracking_to_po, args):
-  print("Filling purchase orders")
-
-  for cluster in all_clusters:
-    if args.groups and cluster.group not in args.groups:
-      continue
-
-    #cluster.non_reimbursed_trackings = set(cluster.trackings)
-    for tracking in cluster.trackings:
-      if tracking in tracking_to_po:
-        cluster.purchase_orders.add(tracking_to_po[tracking])
-        #cluster.non_reimbursed_trackings.remove(tracking)
-
 
 if __name__ == "__main__":
-  main()      
+  main()
