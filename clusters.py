aca133f000856f60d18e06d1b65e8c2471092db8
kellynicole1992 <64326430+kellynicole1992@users.noreply.github.com>
2020-05-25 14:32:07 -0400

    Update clusters.py

--- clusters.py
+++ clusters.py
@@ -28,8 +28,8 @@ class Cluster:
                 manual_override=False,
                 non_reimbursed_trackings=set(),
                 cancelled_items=[],
-                last_delivery_date='',
-                below_cost=False,
+                last_delivery_date='',  
+                below_cost=False, 
                 verified=False) -> None:
     self.orders = orders
     self.trackings = trackings
@@ -46,7 +46,7 @@ class Cluster:
     self.non_reimbursed_trackings = non_reimbursed_trackings
     self.cancelled_items = cancelled_items
     self.last_delivery_date = last_delivery_date
-    self.below_cost = below_cost
+    self.below_cost = below_cost  
     self.verified = verified
 
   def __setstate__(self, state) -> None:
@@ -54,28 +54,25 @@ class Cluster:
 
   def __str__(self) -> str:
     return "orders: %s, trackings: %s, group: %s, expected cost: %s, tracked cost: %s, last_ship_date: %s, last_delivery_date: %s, purchase_orders: %s, email_ids: %s, adjustment: %s" % (
-        str(self.orders), str(self.trackings), self.group,
-        str(self.expected_cost), str(
-            self.tracked_cost), self.last_ship_date, self.last_delivery_date,
+        str(self.orders), str(self.trackings), self.group, str(self.expected_cost),
+        str(self.tracked_cost), self.last_ship_date, self.last_delivery_date,
         str(self.purchase_orders), str(self.email_ids), str(self.adjustment))
 
   def get_header(self) -> List[str]:
     return [
         "Orders", "Trackings", "To Email", "Amount Billed", "Amount Reimbursed",
-        "Non-Reimbursed Trackings", "Last Ship Date",
-        "Last Delivery Date (Est.)", "POs", "Group", "Manual Cost Adjustment",
-        "Manual Override", "Total Diff","Verified", "Notes", "Cancelled Items", "Below Cost"
+        "Non-Reimbursed Trackings", "Last Ship Date", "Last Delivery Date (Est.)", "POs", "Group",
+        "Manual Cost Adjustment", "Manual Override", "Total Diff", "Verified", "Notes", "Cancelled Items", "Below Cost"
     ]
 
   def to_row(self) -> list:
     return [
-        ", ".join(self.orders), ", ".join(self.trackings), self.to_email,
-        self.expected_cost, self.tracked_cost,
-        ", ".join(self.non_reimbursed_trackings), self.last_ship_date,
-        self.last_delivery_date, "'" + ", ".join(self.purchase_orders),
-        self.group, self.adjustment, self.manual_override,
-        '=INDIRECT(CONCAT("D", ROW())) - INDIRECT(CONCAT("E", ROW())) - INDIRECT(CONCAT("K", ROW()))', self.verified,
-        self.notes, ", ".join(self.cancelled_items), self.below_cost
+        ", ".join(self.orders), ", ".join(self.trackings), self.to_email, self.expected_cost,
+        self.tracked_cost, ", ".join(self.non_reimbursed_trackings), self.last_ship_date,
+        self.last_delivery_date, "'" + ", ".join(self.purchase_orders), self.group, self.adjustment,
+        self.manual_override,
+        '=INDIRECT(CONCAT("D", ROW())) - INDIRECT(CONCAT("E", ROW())) - INDIRECT(CONCAT("K", ROW()))',
+        self.verified, self.notes, ", ".join(self.cancelled_items), self.below_cost
     ]
 
   def merge_with(self, other) -> None:
@@ -86,8 +83,7 @@ class Cluster:
     self.expected_cost += other.expected_cost
     self.tracked_cost += other.tracked_cost
     self.last_ship_date = max(self.last_ship_date, other.last_ship_date)
-    self.last_delivery_date = max(self.last_delivery_date,
-                                  other.last_delivery_date)
+    self.last_delivery_date = max(self.last_delivery_date, other.last_delivery_date)
     self.purchase_orders.update(other.purchase_orders)
     self.email_ids.update(other.email_ids)
     self.adjustment += other.adjustment
@@ -101,7 +97,7 @@ class Cluster:
       self.manual_override = False
     self.non_reimbursed_trackings.update(other.non_reimbursed_trackings)
     self.cancelled_items.extend(other.cancelled_items)
-    self.below_cost = False
+    self.below_cost = False 
     self.verified = False
 
 
@@ -127,15 +123,13 @@ def update_clusters(all_clusters, trackings) -> None:
       if cluster.manual_override:
         override_overridden = True
       cluster.manual_override = False
-      cluster.below_cost = False
-      cluster.verified = False
     cluster.orders.update(tracking.order_ids)
     cluster.trackings.add(tracking.tracking_number)
-    cluster.last_ship_date = max(cluster.last_ship_date,
-                                 str(tracking.ship_date))
-    cluster.last_delivery_date = max(cluster.last_delivery_date,
-                                     str(tracking.delivery_date))
+    cluster.last_ship_date = max(cluster.last_ship_date, str(tracking.ship_date))
+    cluster.last_delivery_date = max(cluster.last_delivery_date, str(tracking.delivery_date))
     cluster.to_email = tracking.to_email
+    cluster.below_cost = False  
+    cluster.verified = False
     if override_overridden:
       print(f"Cluster {cluster.orders} manual override unset because of newly "
             "added trackings or orders.")
@@ -169,12 +163,9 @@ def find_by_shared_attr(cluster, all_clusters) -> Any:
 
   for candidate in all_clusters:
     if candidate.group == cluster.group:
-      common_pos = candidate.purchase_orders.intersection(
-          cluster.purchase_orders)
+      common_pos = candidate.purchase_orders.intersection(cluster.purchase_orders)
       if common_pos:
-        print(
-            f'Merged orders {cluster.orders} and {candidate.orders} by common POs {common_pos}'
-        )
+        print(f'Merged orders {cluster.orders} and {candidate.orders} by common POs {common_pos}')
         return candidate
       common_emails = candidate.email_ids.intersection(cluster.email_ids)
       if common_emails:
@@ -185,33 +176,26 @@ def find_by_shared_attr(cluster, all_clusters) -> Any:
 
 def from_row(header, row) -> Cluster:
   if 'Orders' in header:
-    orders = set(
-        [o.strip() for o in str(row[header.index('Orders')]).split(',')])
+    orders = set([o.strip() for o in str(row[header.index('Orders')]).split(',')])
   else:
     orders = set()
 
   if 'Trackings' in header:
-    trackings = set(
-        [t.strip() for t in str(row[header.index('Trackings')]).split(',')])
+    trackings = set([t.strip() for t in str(row[header.index('Trackings')]).split(',')])
   else:
     trackings = set()
 
-  expected_cost_str = row[header.index(
-      'Amount Billed')] if 'Amount Billed' in header else ''
+  expected_cost_str = row[header.index('Amount Billed')] if 'Amount Billed' in header else ''
   expected_cost = float(expected_cost_str) if expected_cost_str else 0.0
-  tracked_cost_str = row[header.index(
-      "Amount Reimbursed")] if "Amount Reimbursed" in header else ''
+  tracked_cost_str = row[header.index("Amount Reimbursed")] if "Amount Reimbursed" in header else ''
   tracked_cost = float(tracked_cost_str) if tracked_cost_str else 0.0
-  non_reimbursed_str = str(row[header.index("Non-Reimbursed Trackings")]
-                          ) if "Non-Reimbursed Trackings" in header else ""
-  non_reimbursed_trackings = set([
-      t.strip() for t in non_reimbursed_str.split(',')
-  ]) if non_reimbursed_str else set()
-  last_ship_date = row[header.index(
-      'Last Ship Date')] if 'Last Ship Date' in header else '0'
-  last_delivery_date = row[
-      header.index('Last Delivery Date (Est.)'
-                  )] if 'Last Delivery Date (Est.)' in header else ''
+  non_reimbursed_str = str(
+      row[header.index("Non-Reimbursed Trackings")]) if "Non-Reimbursed Trackings" in header else ""
+  non_reimbursed_trackings = set([t.strip() for t in non_reimbursed_str.split(',')
+                                 ]) if non_reimbursed_str else set()
+  last_ship_date = row[header.index('Last Ship Date')] if 'Last Ship Date' in header else '0'
+  last_delivery_date = row[header.index(
+      'Last Delivery Date (Est.)')] if 'Last Delivery Date (Est.)' in header else ''
   pos_string = str(row[header.index('POs')]) if 'POs' in header else ''
   pos = set([s.strip() for s in pos_string.split(',')]) if pos_string else set()
   email_ids = set()  # Set this if we want email IDs in the Sheet
@@ -219,21 +203,19 @@ def from_row(header, row) -> Cluster:
   adj_string = row[header.index(
       "Manual Cost Adjustment")] if "Manual Cost Adjustment" in header else ''
   adjustment = float(adj_string) if adj_string else 0.0
-  manual_override = row[header.index(
-      'Manual Override')] if 'Manual Override' in header else False
+  manual_override = row[header.index('Manual Override')] if 'Manual Override' in header else False
   to_email = row[header.index('To Email')] if 'To Email' in header else ''
   notes = str(row[header.index('Notes')]) if 'Notes' in header else ''
-  cancelled_items_str = str(row[header.index(
-      "Cancelled Items")]) if "Cancelled Items" in header else ""
+  cancelled_items_str = str(
+      row[header.index("Cancelled Items")]) if "Cancelled Items" in header else ""
   cancelled_items = [i.strip() for i in cancelled_items_str.split(',')
                     ] if cancelled_items_str else []
-  below_cost = row[header.index(
-      'Below Cost')] if 'Below Cost' in header else False
-  verified = row[header.index(
-      'Verified')] if 'Verified' in header else False
+  below_cost = row[header.index(  
+        'Below Cost')] if 'Below Cost' in header else False 
+  verified = row[header.index(  
+        'Verified')] if 'Verified' in header else False
   cluster = Cluster(group)
-  cluster._initiate(orders, trackings, group, expected_cost, tracked_cost,
-                    last_ship_date, pos, email_ids, adjustment, to_email, notes,
-                    manual_override, non_reimbursed_trackings, cancelled_items,
-                    last_delivery_date, below_cost, verified)
+  cluster._initiate(orders, trackings, group, expected_cost, tracked_cost, last_ship_date, pos,
+                    email_ids, adjustment, to_email, notes, manual_override,
+                    non_reimbursed_trackings, cancelled_items, last_delivery_date, below_cost, verified)
   return cluster
